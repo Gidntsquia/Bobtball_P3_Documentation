48e4dfbdd42f048357ad0c98ea345dd408fdbed8
Gidntsquia <32310846+Gidntsquia@users.noreply.github.com>
2018-10-19 17:51:23 -0400

    Added functionality for sensor file

--- Legobot Code/utils.py
+++ Legobot Code/utils.py
@@ -114,8 +114,8 @@ def calibrate():
     c.MIN_TOPHAT_VALUE_RIGHT = min_sensor_value_right
     c.MAX_TOPHAT_VALUE_LEFT = max_sensor_value_left
     c.MIN_TOPHAT_VALUE_LEFT = min_sensor_value_left
-    f.backwards_through_two_lines_in_calibration()
-    f.align_close_smart()
+    s.backwards_through_two_lines_in_calibration()
+    s.align_close_smart()
     m.backwards(600)
     msleep(25)
     ao()
