545a79e2ef6322e7aa59db7f98ce8ac95765f9a5
Gidntsquia <32310846+Gidntsquia@users.noreply.github.com>
2018-10-19 21:59:23 -0400

    Got rid of codes from last year

--- Roomba Code/main.py
+++ Roomba Code/main.py
@@ -11,21 +11,9 @@ import utils as u
 def main():
     print "Starting main()\n"
     u.setup()
-    u.calibrate()
-    a.only_first_three()
-    # m.backwards(1300)
-    m.turn_right()
-    m.backwards(4500)
-    u.sd()
-
-
-    a.get_low_poms_cheeky()
-    a.get_frisbee()
-    a.get_mid_poms()
-    a.get_high_poms_cheeky()
-    a.get_farther_high_poms() 
-    a.get_farther_mid_poms()
-    a.get_farther_low_poms()
+    # u.calibrate()  # You only need to include this command if you want the tophats to sense better at the cost of speed.
+    
+    
     print "Finished main\n"
     u.shutdown(86)
     
