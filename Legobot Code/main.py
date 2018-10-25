0b48c87d3f58964609343dc6d950956d77475533
Gidntsquia <32310846+Gidntsquia@users.noreply.github.com>
2018-10-25 16:13:41 -0400

    Tested and fixed on actual robot

--- Legobot Code/main.py
+++ Legobot Code/main.py
@@ -12,9 +12,19 @@ import utils as u
 def main():
     print "Starting main()\n"
     u.setup()
-    # u.calibrate()  # You only need to include this command if you want the tophats to sense better at the cost of speed.
-    
-    
+    u.calibrate()  # You only need to include this command if you want the tophats to sense better at the cost of speed.
+    s.drive_until_black_left(10000, False)
+    s.drive_until_white_left(10000, False)
+    s.align_far_smart()
+    s.drive_until_black_third(10000, False)
+    s.drive_until_white_third(10000, False)
+    s.drive_until_black_third(10000, False)
+    s.drive_until_white_third()
+    s.left_point_turn_until_black()
+    s.pid_lfollow_left_until_right_senses_black()
+    m.drive(500)
+    m.turn_left()
+    s.align_far_smart()
     u.shutdown()
 
 
