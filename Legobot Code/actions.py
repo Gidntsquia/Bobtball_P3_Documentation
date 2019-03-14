b95f14f19aec7c3269c0a695f63e3f8a0f75ec62
LucasMFranke <32496654+LucasMFranke@users.noreply.github.com>
2019-03-14 16:17:42 -0400

    Update, made sure everything is in order

--- Legobot Code/actions.py
+++ Legobot Code/actions.py
@@ -7,13 +7,17 @@ import sensors as s
 import utils as u
 import webcam as w
 
-def get_firefighters():
-    m.turn_left()
-    s.drive_until_black()
-    s.smart_close_align()
-    s.drive_through_two_lines_third()
-    s.turn_left_until_left_black()
-    s.lfollow_left_until_right_senses_black()
+far_safe_zone = False
+
+
+
+def ambulance_guy():
+    s.drive_through_line_third(0)
+    s.drive_through_line_third()
+    s.turn_left_until_black()
+    print "turned"
+    #s.lfollow_left_until_right_senses_black_smooth()
+    print "done"
 
 
 def deliver_ambulance():
@@ -25,20 +29,22 @@ def deliver_ambulance():
     s.turn_right_until_black()
     s.lfollow_right_until_left_senses_black_smooth()
     w.check_zones_hospital()
+    #c.SAFE_HOSPITAL == c.FAR_ZONE:
+
     if c.SAFE_HOSPITAL == c.FAR_ZONE:
-        s.drive_through_line_left()
+        s.drive_through_line_left(0)
         s.drive_until_black_third()
         s.turn_right_until_black()
         s.lfollow_right_until_left_senses_black_smooth()
-        s.drive_through_line_left()
+        s.drive_through_line_left(0)
         s.drive_until_black_third()
         s.turn_right_until_black(0)
         s.turn_right_until_white(0)
         s.turn_right_until_black(0)
         s.turn_right_until_black(0)
-        s.turn_right_until_white(0)
+        s.turn_right_until_white()
         m.lower_arm()
-        m.drive(700)
+        s.drive_until_black_left()
     else:
         s.drive_through_line_left()
         m.drive(300)
@@ -47,5 +53,16 @@ def deliver_ambulance():
         s.turn_left_until_right_senses_white(0)
         s.turn_left_until_black()
         m.lower_arm()
-        m.drive(700)
+        s.drive_until_black_left()
         s.backwards_through_line_third()
+
+        
+            
+def fireFighters():
+    if c.SAFE_HOSPITAL == c.FAR_ZONE:
+        s.lfollow_left_until_right_senses_black(0)
+        s.drive_through_line_right()
+        
+    else:
+        pass
+            
