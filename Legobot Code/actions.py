7b2508565015db91f1ab5a8f5cb38a70ff580c75
LucasMFranke <32496654+LucasMFranke@users.noreply.github.com>
2019-03-20 15:00:26 -0400

    Update actions.py

--- Legobot Code/actions.py
+++ Legobot Code/actions.py
@@ -7,19 +7,6 @@ import sensors as s
 import utils as u
 import webcam as w
 
-far_safe_zone = False
-
-
-
-def ambulance_guy():
-    s.drive_through_line_third(0)
-    s.drive_through_line_third()
-    s.turn_left_until_black()
-    print "turned"
-    #s.lfollow_left_until_right_senses_black_smooth()
-    print "done"
-
-
 def deliver_ambulance():
     u.enable_servo(c.CLAW_SERVO)
     m.lower_arm()
@@ -29,8 +16,7 @@ def deliver_ambulance():
     s.turn_right_until_black()
     s.lfollow_right_until_left_senses_black_smooth()
     w.check_zones_hospital()
-    #c.SAFE_HOSPITAL == c.FAR_ZONE:
-
+    #c.SAFE_HOSPITAL = c.FAR_ZONE
     if c.SAFE_HOSPITAL == c.FAR_ZONE:
         s.drive_through_line_left(0)
         s.drive_until_black_third()
@@ -53,16 +39,26 @@ def deliver_ambulance():
         s.turn_left_until_right_senses_white(0)
         s.turn_left_until_black()
         m.lower_arm()
-        s.drive_until_black_left()
+        #s.drive_until_black_left()
         s.backwards_through_line_third()
 
-        
             
-def fireFighters():
+def get_firefighters():
     if c.SAFE_HOSPITAL == c.FAR_ZONE:
-        s.lfollow_left_until_right_senses_black(0)
-        s.drive_through_line_right()
+        s.drive_through_line_third()
+        s.turn_left_until_black()
+        s.left_forwards_until_white()
+        s.left_forwards_until_black()
         
     else:
-        pass
+        s.turn_right_until_black()
+        s.drive_through_line_left(0)
+        s.drive_through_line_third()
+        s.turn_right_until_black(0)
+        s.turn_right_until_white()
+        #s.drive_until_white_left()
+        #s.turn_right_until_left_senses_black()
+        #s.turn_left_until_left_senses_white()
+        #print "starting ending lfollow"
+        #s.lfollow_left_until_third_senses_black_smooth()
             
